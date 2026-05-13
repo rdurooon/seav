a63321aa79010e8cff4d3535cd6b2345ac248f78
rdurooon <rdurao2005@gmail.com>
2026-05-13 15:18:24 -0300

    V0.0.63 | Hotfix emergencial

--- program/main.py
+++ program/main.py
@@ -1,25 +1,11 @@
 import webview
 import os
 import screeninfo
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 from routes import API
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 src_dir = os.path.join(base_dir, 'src')
 index_inicial = os.path.join(src_dir, 'home.html')
-
-class ReloadHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.src_path.endswith(('.html', '.css', '.js')): # type: ignore
-            print(f"Arquivo modificado, recarregando...")
-            window.evaluate_js('window.location.reload()') # type: ignore
-
-def start_watching():
-    event_handler = ReloadHandler()
-    observer = Observer()
-    observer.schedule(event_handler, src_dir, recursive=True)
-    observer.start()
 
 def center_screen(largura, altura):
     monitor = screeninfo.get_monitors()[0]
@@ -47,5 +33,4 @@ if __name__ == '__main__':
     )
 
     api.set_window(window)
-    start_watching()
     webview.start()
