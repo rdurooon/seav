1ad71527d49badf452d4340aee69dfce07482534
M0b1el <duartemarquesl68@gmail.com>
2026-05-20 22:02:56 -0300

    V0.0.70 | Fix: modal-adicionar restaurado, sistema de classes nos modais, bugs de pesquisa e hitbox corrigidos

--- program/routes.py
+++ program/routes.py
@@ -208,3 +208,6 @@ class API:
         for porta in portas:
             portas_disponiveis.append(porta.device)
         return portas_disponiveis
+    
+    def get_status(self):
+     return "Sem Conexão"
